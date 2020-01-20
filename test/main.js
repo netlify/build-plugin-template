26474dbcc4bc0d48c1f85d7b413a1c143cbbd28e
ehmicky <ehmicky@gmail.com>
2020-01-20 23:18:23 +0100

    Update comment

--- test/main.js
+++ test/main.js
@@ -5,11 +5,11 @@ const HELPERS_DIR = `${__dirname}/helpers`
 const NETLIFY_CONFIG = `${HELPERS_DIR}/netlify.yml`
 
 // Unit tests are using the AVA test runner: https://github.com/avajs/ava
+// A local build is performed using the following command:
+//   netlify-build --config ./helpers/netlify.yml
+// Please see this netlify.yml configuration file. It simply runs the
+// Build plugin.
 test('Netlify Build should not fail', async t => {
-  // A local build is performed using the following command:
-  //   netlify-build --config ./helpers/netlify.yml
-  // Please see this netlify.yml configuration file. It simply runs the
-  // Build plugin.
   const childProcess = execa('netlify-build', ['--config', NETLIFY_CONFIG], {
     // `FORCE_COLOR` is needed to show colors in the terminal
     env: { FORCE_COLOR: '1' },
