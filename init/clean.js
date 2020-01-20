782b99015e9ad881e99dbdb41115d8828ce183b9
ehmicky <ehmicky@gmail.com>
2020-01-20 18:45:51 +0100

    Remove dead code

--- init/clean.js
+++ init/clean.js
@@ -7,7 +7,6 @@ const omit = require('omit.js')
 const PACKAGE_ROOT = `${__dirname}/../..`
 const SCRIPTS_DIR = `${PACKAGE_ROOT}/init`
 const PACKAGE_JSON = `${PACKAGE_ROOT}/package.json`
-const README = `${PACKAGE_ROOT}/README.md`
 
 const pReadFile = promisify(readFile)
 const pWriteFile = promisify(writeFile)
@@ -15,11 +14,7 @@ const pWriteFile = promisify(writeFile)
 // Remove all files, properties and logic needed by `npm run init` once
 // `npm run init` is done.
 const cleanRepo = async function() {
-  await Promise.all([
-    del(SCRIPTS_DIR, { force: true }),
-    cleanPackageJson(),
-    cleanReadme(),
-  ])
+  await Promise.all([del(SCRIPTS_DIR, { force: true }), cleanPackageJson()])
 }
 
 // Remove `npm run init` in `package.json` and all `devDependencies`.
@@ -34,14 +29,5 @@ const cleanPackageJson = async function() {
   const contentA = JSON.stringify(packageJsonB, null, 2)
   await pWriteFile(PACKAGE_JSON, contentA)
 }
-
-// Remove instructions about `npm run init` from the `README`
-const cleanReadme = async function() {
-  const content = await pReadFile(README, 'utf8')
-  const contentA = content.replace(README_REGEXP, '')
-  await pWriteFile(README, contentA)
-}
-
-const README_REGEXP = /\n<!-- START -->[^]*<!-- END -->\n/
 
 module.exports = { cleanRepo }
