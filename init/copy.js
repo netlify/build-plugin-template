fef7670fc5120602747007e0d5de99fd32842d20
ehmicky <ehmicky@gmail.com>
2020-01-20 18:45:02 +0100

    Add comment

--- init/copy.js
+++ init/copy.js
@@ -2,6 +2,7 @@ const cpy = require('cpy')
 
 const PACKAGE_ROOT = `${__dirname}/..`
 
+// Copy some files during initialization
 const copyFiles = async function() {
   const files = FILES.map(file => `${__dirname}/${file}`)
   await cpy(files, PACKAGE_ROOT)
