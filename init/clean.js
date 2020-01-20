d66a3f88c1ea89eff917fc768032cfc81d8a23a3
ehmicky <ehmicky@gmail.com>
2020-01-20 18:49:44 +0100

    Clean .eslintrc.yml

--- init/clean.js
+++ init/clean.js
@@ -7,6 +7,7 @@ const omit = require('omit.js')
 const PACKAGE_ROOT = `${__dirname}/../..`
 const SCRIPTS_DIR = `${PACKAGE_ROOT}/init`
 const PACKAGE_JSON = `${PACKAGE_ROOT}/package.json`
+const ESLINTRC = `${PACKAGE_ROOT}/.eslintrc.yml`
 
 const pReadFile = promisify(readFile)
 const pWriteFile = promisify(writeFile)
@@ -14,7 +15,11 @@ const pWriteFile = promisify(writeFile)
 // Remove all files, properties and logic needed by `npm run init` once
 // `npm run init` is done.
 const cleanRepo = async function() {
-  await Promise.all([del(SCRIPTS_DIR, { force: true }), cleanPackageJson()])
+  await Promise.all([
+    del(SCRIPTS_DIR, { force: true }),
+    cleanPackageJson(),
+    cleanEslintrc(),
+  ])
 }
 
 // Remove `npm run init` in `package.json` and all `devDependencies`.
@@ -29,5 +34,14 @@ const cleanPackageJson = async function() {
   const contentA = JSON.stringify(packageJsonB, null, 2)
   await pWriteFile(PACKAGE_JSON, contentA)
 }
+
+// Remove init-specific ESLint rules
+const cleanEslintrc = async function() {
+  const content = await pReadFile(ESLINTRC, 'utf8')
+  const contentA = content.replace(ESLINTRC_REGEXP, '')
+  await pWriteFile(ESLINTRC, contentA)
+}
+
+const ESLINTRC_REGEXP = /override:[^]*/
 
 module.exports = { cleanRepo }
